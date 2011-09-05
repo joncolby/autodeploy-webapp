2f284fe458e9a875f997d8a0329c78dcbf254c8e
Ingo Bartel <ibartel@ebay.de>
2011-09-05 16:17:49 +0200

    added deploy action to api

--- grails-app/controllers/de/mobile/siteops/ApiController.groovy
+++ grails-app/controllers/de/mobile/siteops/ApiController.groovy
@@ -4,6 +4,8 @@ class ApiController {
 
     def deploymentPlanService
 
+    def deploymentQueueService
+
     def modulesForPlan = {
         def planId = params.planId
         if (planId) {
@@ -28,14 +30,24 @@ class ApiController {
         render "status:$result.type\nmessage:$result.message\nqueueEntryId:$result.queueEntryId"
     }
 
-    def deployPlan = {
-        def planId = params.id
-        if (planId) {
-            def plan = DeploymentPlan.get(planId)
-            if (plan) {
-
-            }
+    def deploy = {
+        def queueEntryId = params.queueEntryId
+        def queueEntry = DeploymentQueueEntry.findById(queueEntryId)
+        def canDeploy = deploymentQueueService.canDeploy(queueEntry)
+        if (!canDeploy.success) {
+            render "status:error\nmessage:$canDeploy.message"
+            return
         }
+
+        def deployedHosts = deploymentQueueService.createDeployedHostsForQueueEntry(queueEntry)
+        if (!deployedHosts) {
+            render "status:error\nmessage:Could not find any deployable hosts"
+            return
+        }
+
+        deploymentQueueService.deployNextHosts(queueEntry)
+
+        render "status:succcess\nmessage:deployment for '$queueEntry.executionPlan.name' started."
     }
 
 }
