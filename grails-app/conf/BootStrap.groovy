1c93dc853e308002516f14d8bd3f22d91a08753c
Jonathan Colby <jcolby@ebay.de>
2012-05-07 17:49:06 +0200

    no auto-role creation in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,9 +15,9 @@ class BootStrap {
         zookeeperHandlerService.init()
 
         def adminRole = SecRole.findByAuthority('ROLE_ADMIN') ?: new SecRole(authority: 'ROLE_ADMIN').save(failOnError: true)
-        def userRole = SecRole.findByAuthority('ROLE_USER') ?: new SecRole(authority: 'ROLE_USER').save(failOnError: true)
-        def readProdRole = SecRole.findByAuthority('ROLE_PROD_READ') ?: new SecRole(authority: 'ROLE_PROD_READ').save(failOnError: true)
-        def writeProdRole = SecRole.findByAuthority('ROLE_PROD_WRITE') ?: new SecRole(authority: 'ROLE_PROD_WRITE').save(failOnError: true)
+        //def userRole = SecRole.findByAuthority('ROLE_USER') ?: new SecRole(authority: 'ROLE_USER').save(failOnError: true)
+        //def readProdRole = SecRole.findByAuthority('ROLE_PROD_READ') ?: new SecRole(authority: 'ROLE_PROD_READ').save(failOnError: true)
+        //def writeProdRole = SecRole.findByAuthority('ROLE_PROD_WRITE') ?: new SecRole(authority: 'ROLE_PROD_WRITE').save(failOnError: true)
 
         def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
                 username: 'admin',
