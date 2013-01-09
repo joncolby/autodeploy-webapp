c431aba2ef267047dc9e616df58b17530286dfc0
Jonathan Colby <jcolby@ebay.de>
2013-01-09 12:43:14 +0100

    fix cleanup job to deal with deleted applications

--- grails-app/services/de/mobile/siteops/ApplicationService.groovy
+++ grails-app/services/de/mobile/siteops/ApplicationService.groovy
@@ -1,5 +1,8 @@
 package de.mobile.siteops
 
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.codehaus.groovy.grails.commons.DomainClassArtefactHandler
+
 class ApplicationService {
 
     static transactional = false
@@ -40,7 +43,6 @@ class ApplicationService {
         def queueEntries = DeploymentQueueEntry.finalizedEntries(deploymentQueue).list(sort: 'finalizedDate', order: 'desc')
 
         def apps = Application.findAll()
-
         apps.each { app ->
             result += new ApplicationVersion(application: app, revision: null)
         }
@@ -51,7 +53,22 @@ class ApplicationService {
             def deployedHosts = DeployedHost.findAllByEntryAndState(queueEntry, HostStateType.DEPLOYED)
             def previousApplications = []
             deployedHosts.each { deployedHost ->
-                deployedHost.host.className.applications.intersect(entryApplications.collect { it.application }).each { app ->
+
+                /*  get all the applications associated with this, using criteria.
+                    Necessary because there may be an application that no longer exists
+                    in the host_class_applications mapping table.  For example, if someone deletes the application
+                    inappropriately by hand.
+                */
+                 def applicationsInHostClass = Application.createCriteria().list() {
+                     hostclasses {
+                         eq('id', deployedHost.host.className.id)
+                     }
+                 }
+
+                // this fails if a mapped application in host_class_applications no longer exists
+                //deployedHost.host.className.applications.intersect(entryApplications.collect {
+
+                applicationsInHostClass.intersect(entryApplications.collect { it.application }).each { app ->
                     previousApplications += entryApplications.findAll { it.application == app }
                 }
             }
